706a2571e8f7ee2d3e2f9b5a2be451d248e3f756
Rahul Gandhi <rahulgandhi@192.168.1.124>
2025-08-29 23:05:41 +0100

    add prettier and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ const products = await ProductAPI.getProducts();
 
 function App() {
   return (
-    <>
-      <div>Read README.md</div>
+    <div className="flex flex-col gap-3">
+      <h1 className="text-3xl font-bold underline text-center p-4 border-gray-100 border-8">
+        Hello world!
+      </h1>
       <div>Number of products: {products.length}</div>
-    </>
+    </div>
   );
 }
 
